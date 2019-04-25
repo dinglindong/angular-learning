4c130a2a3d05ed2471c35c0c93899bdda7975974
dinglindong <258231674@qq.com>
2019-04-25 21:08:03 +0800

    add desc

---
+++ src/modules/autologout/index.vue
@@ -0,0 +1,22 @@
+<template>
+    <section class="index">
+        
+    </section>
+</template>
+
+<script>
+    /**
+     * @author You name (*必填)
+     * @date 2019/4/25
+     * @desc 组件描述
+     * @param {Object} [title]  - 参数说明
+     */
+
+    export default {
+        name: "index"
+    }
+</script>
+
+<style rel="stylesheet/scss" lang="scss" scoped>
+
+</style>
